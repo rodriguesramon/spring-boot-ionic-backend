94a9afae4ce8b4cf63223be3f07057cc32488a12
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2019-08-23 01:23:40 -0300

    Testando a Primeira Classe de Domínio

--- src/main/java/com/ramoncosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ramoncosta/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.ramoncosta.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.ramoncosta.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest rodando...";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(categoria1);
+		lista.add(categoria2);
+		return lista;
 	}
 }
