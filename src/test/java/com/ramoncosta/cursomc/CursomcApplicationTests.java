48dc3ac4b054db7451254f34eeb1eb7913e7374c
unknown <ramon.rodrigues.lima@gmail.com>
2019-08-23 00:46:31 -0300

    Projeto Criado

---
+++ src/test/java/com/ramoncosta/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramoncosta.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
