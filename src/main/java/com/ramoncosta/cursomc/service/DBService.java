d8ec3dc1c29d8faaaf35b75a5907fce6aecdecd8
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2019-10-20 01:02:00 -0400

    Criando Profile de Desenvolvimento

--- src/main/java/com/ramoncosta/cursomc/service/DBService.java
+++ src/main/java/com/ramoncosta/cursomc/service/DBService.java
@@ -61,6 +61,8 @@ public class DBService {
 		Categoria categoria6 = new Categoria(null, "Decoração");
 		Categoria categoria7 = new Categoria(null, "Perfumaria");
 		Categoria categoria8 = new Categoria(null, "Infantil");
+		Categoria categoria9 = new Categoria(null, "Test");
+		Categoria categoria10 = new Categoria(null, "TestNew");
 		
 		Produto produto1 = new Produto(null, "Computador", 2000.00);
 		Produto produto2 = new Produto(null, "Impressora", 1000.00);
@@ -94,7 +96,7 @@ public class DBService {
 		produto10.getCategorias().addAll(Arrays.asList(categoria6));
 		produto11.getCategorias().addAll(Arrays.asList(categoria7));
 		
-		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7, categoria8));
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7, categoria8, categoria9, categoria10));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11));
 
 		//########################################################################################
