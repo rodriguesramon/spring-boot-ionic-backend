4dd648cf0ea8c1ece100d46f4e9a1781eb17bbd3
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2019-10-23 21:03:15 -0400

    Implementando SmtpEmailService com Servidor do Google

--- src/main/java/com/ramoncosta/cursomc/config/DevConfig.java
+++ src/main/java/com/ramoncosta/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ramoncosta.cursomc.service.DBService;
+import com.ramoncosta.cursomc.service.EmailService;
+import com.ramoncosta.cursomc.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
