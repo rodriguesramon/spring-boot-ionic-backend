38919b29d6d25ee16b3d3d7fd1eba91ced42e677
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2019-10-25 15:39:27 -0400

    Autorizando Endpoints para Perfis Específicos

--- src/main/java/com/ramoncosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ramoncosta/cursomc/resources/CategoriaResource.java
@@ -3,10 +3,13 @@ package com.ramoncosta.cursomc.resources;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.ramoncosta.cursomc.domain.Categoria;
 import com.ramoncosta.cursomc.dto.CategoriaDTO;
 import com.ramoncosta.cursomc.service.CategoriaService;
@@ -31,6 +35,7 @@ public class CategoriaResource {
 	   	return ResponseEntity.ok().body(categoria);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
 		Categoria categoria = categoriaService.fromDTO(categoriaDTO);
@@ -42,6 +47,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();		
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDTO, @PathVariable Integer id){
 		Categoria categoria = categoriaService.fromDTO(categoriaDTO);
@@ -50,6 +56,7 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
