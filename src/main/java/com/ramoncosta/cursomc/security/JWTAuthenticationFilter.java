6fed83c87bfa5caeceddbe3051391daa3ca169ee
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2019-12-19 00:03:26 -0400

    Expondo o header Authorization

--- src/main/java/com/ramoncosta/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/ramoncosta/cursomc/security/JWTAuthenticationFilter.java
@@ -50,6 +50,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) authentication.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		response.addHeader("Auhtorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
